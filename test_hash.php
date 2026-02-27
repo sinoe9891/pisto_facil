a25fa157d80462e2e645567e8ae0d13a86c0f046
Danny Velasquez <sinoeproducciones@gmail.com>
2026-02-26 21:39:04 -0600

    primer commit

--- test_hash.php
+++ test_hash.php
@@ -1,3 +1,12 @@
 <?php
 echo password_hash('Admin@1234', PASSWORD_BCRYPT) . PHP_EOL; echo '<hr>';
 echo password_hash('Stark989121*#', PASSWORD_BCRYPT) . PHP_EOL;
+
+User: superadmin@pistofacil.com
+Pass: Stark989121*#
+
+User: admin@pistofacil.com
+Pass: Admin@1234
+
+User: asesor@pistofacil.com
+Pass: Admin@1234
